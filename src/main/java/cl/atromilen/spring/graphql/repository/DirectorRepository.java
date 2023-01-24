d8a73e8ef3034a14d540c3e48f5fbba82f236ed9
Álvaro Tromilen <alvaro.tromilen@gmail.com>
2023-01-24 04:45:12 -0300

    GraphQL API - movies
    - Added GraphQL dependencies in build.gradle
    - Added GraphQL schema with types Query, Movie and Director
    - Added GraphQL controller
    - Added service layer for Movie and Director entities
    - Enabled UI tool GraphiQL in application file
    - Added README

--- src/main/java/cl/atromilen/spring/graphql/repository/DirectorRepository.java
+++ src/main/java/cl/atromilen/spring/graphql/repository/DirectorRepository.java
@@ -2,8 +2,6 @@ package cl.atromilen.spring.graphql.repository;
 
 import cl.atromilen.spring.graphql.model.Director;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
 
 public interface DirectorRepository extends ReactiveCrudRepository<Director, Integer> {
-    Mono<Director> findByName(String name);
 }
